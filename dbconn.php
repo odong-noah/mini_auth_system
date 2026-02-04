0a9a681dedc744073e25c819447a0a3b2b981cbe
odong-noah <noahodong6@gmail.com>
2026-02-04 14:32:58 +0300

    Initial commit

---
+++ dbconn.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+date_default_timezone_set('America/Los_Angeles');
+
+$dbname = "interview_test";
+$servername = "localhost";
+$Username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $Username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("ERROR J3DJS7SGHSJS");
+}
+
+function clean_string($string_val) {
+    if ($string_val === null) { return ""; }
+    return strip_tags(filter_var($string_val, FILTER_SANITIZE_SPECIAL_CHARS));
+}
